b9b92a472b435d0b3e6b41f7e6162bc66ba89889
vanja.culum <vanja.culum@geneza.com>
2024-06-21 12:31:10 +0200

    build tool server development command added

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,15 +1,15 @@
 import express from 'express'
 import cors from 'cors'
+// "server:start": "nodemon server/src/main.ts"
 
 const host = process.env.HOST ?? 'localhost'
 const port = process.env.PORT ? Number(process.env.PORT) : 8080
 
 const app = express()
-
 app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send({ message: 'Vanjass sAPI' })
+  res.send({ message: 'Testing' })
 })
 
 app.listen(port, host, () => {
